8cdf049d9fa926cbf69e6149cd665e514721b07b
gigDevelopment10 <84071291+gigDevelopment10@users.noreply.github.com>
2022-03-12 23:30:57 +0530

    basic changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+
+import { Heading }  from '@chakra-ui/react';
+import ToDolist from './components/ToDolist';
+import ToDo from './components/ToDo';
+import { VStack } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack>
+     <Heading>To Do app</Heading>
+    </VStack>
   );
 }
 
---
+++ src/components/ToDolist.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function ToDolist() {
+  return (
+    <div>ToDolist</div>
+  )
+}
+
+export default ToDolist;
