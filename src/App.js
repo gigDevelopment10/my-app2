38b1b09245a425bff0826625f53b1fc6bdcafa31
gigDevelopment10 <84071291+gigDevelopment10@users.noreply.github.com>
2022-03-13 12:51:07 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Heading } from '@chakra-ui/react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import ToDoList from './components/ToDoList';
+import AddTodo from './components/AddTodo';
+import { VStack , IconButton} from '@chakra-ui/react';
+import {FaSun, FaMoon} from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <h1>To do app </h1>
-    </div>
+    <VStack p ={4}>
+      <IconButton icon= {<FaSun/>} isRound = 'True' size ="lg" alignSelf='flex-end'/>
+    <Heading mg="8" fontweight="extrabold" size = "2xl" bgGradient ="linear(to-r , pink.500, pink.300, blue.500)"
+    bgClip="text"
+    >
+      To do Application
+    </Heading>
+    </VStack>
+
   );
 }
 
---
+++ src/components/ToDoList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {VStack} from '@chakra-ui/react';
+
+
+function ToDoList() {
+
+    const todos =[
+        id : 1,
+        body: 'get bread',
+    ]
+
+  return (
+    <VStack>
+
+    </VStack>;
+  )
+}
+
+export default ToDoList
